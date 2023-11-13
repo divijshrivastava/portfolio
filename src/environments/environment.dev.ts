2d817117da74d1a4992cb0f2affe3934cb0ac853
Divij <divij.shrivastava@gmail.com>
2023-11-13 20:00:28 +0530

    Adding environment file.

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:80",
+  envName: "dev",
+  color: "green",
+  serverUrl: "http://localhost"
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: "",
+  envName: "prod",
+  color: "green",
+  serverUrl: ""
+};
