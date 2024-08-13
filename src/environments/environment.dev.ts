1bb5d95625b4e5f6d4384e113fc5e7a021046098
Divij <divij.shrivastava@gmail.com>
2024-08-14 05:26:24 +0530

    feature: wrap global window object and make it injectable across app

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:80",
-  envName: "dev",
-  color: "green",
-  serverUrl: "http://localhost"
+  apiUrl: 'http://localhost:80',
+  envName: 'dev',
+  color: 'green',
+  serverUrl: 'http://localhost',
+  recaptcha: {sitekey: '6Les6hQpAAAAAKJV1eW8jR6QRMsqmdOIGPU9-W9J'}
 };
