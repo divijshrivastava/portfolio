e8bbb94e4972393dc61dee8e31b6b21a4079dede
Divij <divij.shrivastava@gmail.com>
2024-10-20 06:17:59 +0530

    (feature): disables config for redirection to port 443

--- src/main/java/tech/divij/PortfolioApplication.java
+++ src/main/java/tech/divij/PortfolioApplication.java
@@ -6,14 +6,8 @@ import com.blueconic.browscap.UserAgentParser;
 import com.blueconic.browscap.UserAgentService;
 import java.io.IOException;
 import java.util.Arrays;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
@@ -29,35 +23,33 @@ public class PortfolioApplication {
     SpringApplication.run(PortfolioApplication.class, args);
   }
 
-  @Bean
-  @Profile("prod")
-  public ServletWebServerFactory servletContainer() {
-    TomcatServletWebServerFactory tomcat =
-        new TomcatServletWebServerFactory() {
-          @Override
-          protected void postProcessContext(Context context) {
-            SecurityConstraint securityConstraint = new SecurityConstraint();
-            securityConstraint.setUserConstraint("CONFIDENTIAL");
-            SecurityCollection collection = new SecurityCollection();
-            collection.addPattern("/*");
-            securityConstraint.addCollection(collection);
-            context.addConstraint(securityConstraint);
-          }
-        };
-    tomcat.addAdditionalTomcatConnectors(redirectConnector());
-    return tomcat;
-  }
+  /**
+   @Bean
+   @Profile("prod") public ServletWebServerFactory servletContainer() {
+   TomcatServletWebServerFactory tomcat =
+   new TomcatServletWebServerFactory() {
+   @Override protected void postProcessContext(Context context) {
+   SecurityConstraint securityConstraint = new SecurityConstraint();
+   securityConstraint.setUserConstraint("CONFIDENTIAL");
+   SecurityCollection collection = new SecurityCollection();
+   collection.addPattern("/*");
+   securityConstraint.addCollection(collection);
+   context.addConstraint(securityConstraint);
+   }
+   };
+   tomcat.addAdditionalTomcatConnectors(redirectConnector());
+   return tomcat;
+   }
 
-  @Bean()
-  @Profile("prod")
-  Connector redirectConnector() {
-    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-    connector.setScheme("http");
-    connector.setPort(80);
-    connector.setSecure(false);
-    connector.setRedirectPort(443);
-    return connector;
-  }
+    *  @Bean()
+   @Profile("prod") Connector redirectConnector() {
+   Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+   connector.setScheme("http");
+   connector.setPort(80);
+   connector.setSecure(false);
+   connector.setRedirectPort(443);
+   return connector;
+   }*/
 
   @Bean
   @Profile("local")
